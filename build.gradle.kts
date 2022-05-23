ab54887ce19a199b66143631d9915ee73351ecbf
Veyndan Stuart <veyndan@gmail.com>
2022-05-23 17:46:05 +0200

    Tests fail with Paging 3

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,8 @@ android {
 }
 
 dependencies {
-  implementation("androidx.paging:paging-common:2.1.2")
-  implementation("androidx.paging:paging-rxjava2:2.1.2")
+  implementation("androidx.paging:paging-common:3.1.1")
+  implementation("androidx.paging:paging-rxjava2:3.1.1")
   implementation("io.reactivex.rxjava2:rxjava:2.2.21")
   testImplementation("junit:junit:4.13.2")
 }
